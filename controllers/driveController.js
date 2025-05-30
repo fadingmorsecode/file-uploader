f7f643ccb3937a7e2671f013825244a7cea00c84
fadingmorsecode <106215095+fadingmorsecode@users.noreply.github.com>
2025-05-30 07:17:41 -0700

    create sorting of uploads

--- controllers/driveController.js
+++ controllers/driveController.js
@@ -3,6 +3,8 @@ const { getFolders } = require('./folderController');
 
 module.exports = async function getUserUploads(userId) {
   const userFiles = await getFiles(userId);
+  userFiles.sort((a, b) => a.name.localeCompare(b.name));
   const userFolders = await getFolders(userId);
+  userFolders.sort((a, b) => a.name.localeCompare(b.name));
   return { files: userFiles, folders: userFolders };
 };
